28c3aeee7fba98594eff2e8923b56fe5e6a29386
Hansura10 <hansura.me@gmail.com>
2025-06-16 02:34:34 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-// Tambahkan animasi transisi dari intro ke countdown lalu ke halaman utama
-function startTransition() {
-  document.getElementById("intro").classList.add("hidden");
-  document.getElementById("countdown").classList.remove("hidden");
-
-  let count = 5;
-  const counter = document.getElementById("count");
-  const interval = setInterval(() => {
-    count--;
-    if (count <= 0) {
-      clearInterval(interval);
-      document.getElementById("countdown").classList.add("hidden");
-      document.getElementById("main").classList.remove("hidden");
-    } else {
-      counter.innerText = count;
-    }
-  }, 1000);
-}
 
 function startTransition() {
   document.getElementById("intro").classList.add("hidden");
@@ -31,17 +13,16 @@ function startTransition() {
       document.getElementById("countdown").classList.add("hidden");
       document.getElementById("main").classList.remove("hidden");
 
-      // BUAT audio dan play hanya setelah semua muncul
+      // Mainkan musik setelah halaman utama tampil
       setTimeout(() => {
         const music = new Audio("assets/musik.mp3");
         music.loop = true;
         music.play().catch((e) => {
           console.warn("Musik gagal diputar:", e);
         });
-      }, 300); // kasih delay sedikit biar transisi terasa
+      }, 300);
     } else {
       counter.innerText = count;
     }
   }, 1000);
 }
-
