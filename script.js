aa4f62c5704ea10fe2e266b414760c10ae95feaa
Hansura10 <hansura.me@gmail.com>
2025-06-22 15:45:05 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -5,6 +5,29 @@ const countdownNumber = document.getElementById("countdown-number");
 const mainContent = document.getElementById("main-content");
 const music = document.getElementById("bg-music");
 
+let carouselImages = [
+  "assets/carousel1.jpg",
+  "assets/carousel2.jpg",
+  "assets/carousel3.jpg"
+];
+let currentSlide = 0;
+const carouselImg = document.getElementById("carousel-image");
+
+function showSlide(index) {
+  if (index < 0) index = carouselImages.length - 1;
+  if (index >= carouselImages.length) index = 0;
+  currentSlide = index;
+  carouselImg.src = carouselImages[currentSlide];
+}
+
+function nextSlide() {
+  showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+  showSlide(currentSlide - 1);
+}
+
 introBtn.addEventListener("click", () => {
   intro.classList.add("hidden");
   countdown.classList.remove("hidden");
@@ -17,7 +40,7 @@ introBtn.addEventListener("click", () => {
       clearInterval(interval);
       countdown.classList.add("hidden");
       mainContent.classList.remove("hidden");
-
+      showSlide(0);
       setTimeout(() => {
         music.play().catch((e) => {
           console.warn("Musik gagal diputar:", e);
